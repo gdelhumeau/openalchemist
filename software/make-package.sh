c56b6365d20617f58f97001b889cef0c8109a61f
k3ph <k3ph@3dcc0fe5-7416-0410-a0d4-eb306e31e832>
2008-05-22 12:13:46 +0000

    Update

--- software/make-package.sh
+++ software/make-package.sh
@@ -18,7 +18,6 @@ cp data/loading.png package/usr/local/games/openalchemist/data/
 cp openalchemist package/usr/local/games/openalchemist/
 #cp openalchemist-static package/usr/local/games/openalchemist/
 cp config.py package/usr/local/games/openalchemist/
-cp openalchemist-config package/usr/local/games/openalchemist/
 echo "Making links"
 ln -s /usr/local/games/openalchemist/openalchemist package/usr/local/bin/openalchemist
 echo "#!/bin/sh" >  package/usr/local/bin/openalchemist-config
