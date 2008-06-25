7909326c0acca224861de4fdf0e0ad9fd635299f
k3ph <k3ph@3dcc0fe5-7416-0410-a0d4-eb306e31e832>
2008-06-25 21:00:39 +0000

    Removing no-usefull files

--- software/make-static.sh
+++
@@ -1,4 +0,0 @@
-echo "Making a lib-independant binary"
-scons
-g++ -o openalchemist-static bin/*.o bin/states/*.o /usr/local/lib/libclanApp.a /usr/local/lib/libclanSDL.a /usr/local/lib/libclanDisplay.a /usr/local/lib/libclanGL.a /usr/local/lib/libclanCore.a /usr/local/lib/libclanSignals.a /usr/lib/libpng.a /usr/lib/libjpeg.a /usr/lib/libz.a /usr/lib/libXmu.a /usr/lib/libXi.a /usr/lib/libXxf86vm.a /usr/lib/libm.a /usr/lib/libGLU.a -lGL `sdl-config --static-libs`  
-echo "OK, result on \"openalchemist-static\""
