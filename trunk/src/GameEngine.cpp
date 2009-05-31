71272e27ad088eca4e4097314a1cc6d09b1e5c98
k3ph <k3ph@3dcc0fe5-7416-0410-a0d4-eb306e31e832>
2009-05-31 00:08:20 +0000

    Adding sounds fx + pause sounds on pause menu

--- trunk/src/GameEngine.cpp
+++ trunk/src/GameEngine.cpp
@@ -7,7 +7,7 @@
   Author : Guillaume Delhumeau <guillaume.delhumeau@gmail.com>
  
  
- *********************************************************************/
+*********************************************************************/
 
 #include "memory.h"
 #include <ClanLib/core.h>
@@ -22,9 +22,9 @@
 
 GameEngine::GameEngine(CL_DisplayWindow *window, bool opengl)
 {
-    this -> _p_window = window;
-    this -> _render_mode = opengl;
-    _p_loading_screen = NULL;
+  this -> _p_window = window;
+  this -> _render_mode = opengl;
+  _p_loading_screen = NULL;
 }
 
 GameEngine::~GameEngine()
@@ -32,112 +32,112 @@ GameEngine::~GameEngine()
 
 void GameEngine::init()
 {
-    _running = true;
+  _running = true;
 	
-		common_resources_init();
-		g_music_manager.init();
+  common_resources_init();
+  g_music_manager.init();
 
-    CommonResources *resources = common_resources_get_instance();
-    Preferences *pref = pref_get_instance();
+  CommonResources *resources = common_resources_get_instance();
+  Preferences *pref = pref_get_instance();
 
-    _fps_getter.set_fps_limit(pref -> maxfps);
-		change_screen_size();
+  _fps_getter.set_fps_limit(pref -> maxfps);
+  change_screen_size();
 
-    resources -> init(this);
-    _common_state.init();
-    _ingame_state.init();
-    _gameover_state.init();
-    _pausemenu_state.init();
-    _skinsmenu_state.init();
-    _optionsmenu_state.init();
-    _title_state.init();
-    _quitmenu_state.init();
+  resources -> init(this);
+  _common_state.init();
+  _ingame_state.init();
+  _gameover_state.init();
+  _pausemenu_state.init();
+  _skinsmenu_state.init();
+  _optionsmenu_state.init();
+  _title_state.init();
+  _quitmenu_state.init();
     
-		set_skin(pref -> skin);
+  set_skin(pref -> skin);
 }
 
 void GameEngine::deinit()
 {
-    _common_state.deinit();
-    _ingame_state.deinit();
-    _gameover_state.deinit();
-    _pausemenu_state.deinit();
-    _skinsmenu_state.deinit();
- 	  _optionsmenu_state.deinit();
-    _title_state.deinit();
-    _quitmenu_state.deinit();
+  _common_state.deinit();
+  _ingame_state.deinit();
+  _gameover_state.deinit();
+  _pausemenu_state.deinit();
+  _skinsmenu_state.deinit();
+  _optionsmenu_state.deinit();
+  _title_state.deinit();
+  _quitmenu_state.deinit();
 		
-		g_music_manager.term();
+  g_music_manager.term();
 }
 
 void GameEngine::run()
 {
-    init();
+  init();
 		
-		Preferences *p_pref = pref_get_instance();	
+  Preferences *p_pref = pref_get_instance();	
 
-    if(_running)
+  if(_running)
+  {
+    set_state_title();
+
+    CommonResources *resources = common_resources_get_instance();
+    resources -> player1.new_game();
+
+    while (_running)
     {
-        set_state_title();
+      _common_state.events();
+      _common_state.update();
+      _common_state.draw();
 
-        CommonResources *resources = common_resources_get_instance();
-        resources -> player1.new_game();
+      GameState* current_state = _states_stack.top();
+      current_state -> events();
+      current_state -> update();
 
-        while (_running)
-        {
-            _common_state.events();
-            _common_state.update();
-            _common_state.draw();
-
-            GameState* current_state = _states_stack.top();
-            current_state -> events();
-            current_state -> update();
-
-            // Drawing the front layer behind the current state or not
-            if (current_state -> front_layer_behind())
-            {
-                resources -> front_layer.draw();
-                current_state -> draw();
-            }
-            else
-            {
-                current_state -> draw();
-                resources -> front_layer.draw();
-            }
+      // Drawing the front layer behind the current state or not
+      if (current_state -> front_layer_behind())
+      {
+	resources -> front_layer.draw();
+	current_state -> draw();
+      }
+      else
+      {
+	current_state -> draw();
+	resources -> front_layer.draw();
+      }
 
 
-            // Get the Framerate
-            resources -> fps = _fps_getter.get_fps();
-            resources -> time_interval = get_time_interval(resources->fps);
+      // Get the Framerate
+      resources -> fps = _fps_getter.get_fps();
+      resources -> time_interval = get_time_interval(resources->fps);
 
-            CL_Display::flip();
+      CL_Display::flip();
 						
-						if(_render_mode == RENDER_OPENGL && !p_pref -> fullscreen)
-						{
-							change_screen_size();
-						}
+      if(_render_mode == RENDER_OPENGL && !p_pref -> fullscreen)
+      {
+	change_screen_size();
+      }
 
-            // This call updates input and performs other "housekeeping"
-            // Call this each frame
-            // Also, gives the CPU a rest for 10 milliseconds to catch up
-            CL_System::keep_alive();
-        }
+      // This call updates input and performs other "housekeeping"
+      // Call this each frame
+      // Also, gives the CPU a rest for 10 milliseconds to catch up
+      CL_System::keep_alive();
     }
+  }
 }
 
 void GameEngine::stop()
 {
-    _running = false;
+  _running = false;
 }
 
 void GameEngine::set_state_title()
 {
-    while (!_states_stack.empty())
-    {
-        _states_stack.pop();
-    }
-    _states_stack.push(&_title_state);
-    _title_state.start();
+  while (!_states_stack.empty())
+  {
+    _states_stack.pop();
+  }
+  _states_stack.push(&_title_state);
+  _title_state.start();
 }
 
 void GameEngine::set_state_new_game_menu()
@@ -145,305 +145,311 @@ void GameEngine::set_state_new_game_menu()
 
 void GameEngine::set_state_pause_menu()
 {
-    if (_states_stack.top() != &_pausemenu_state)
-    {
-        _states_stack.push(&_pausemenu_state);
-        _pausemenu_state.start();
-    }
+  if (_states_stack.top() != &_pausemenu_state)
+  {
+    _states_stack.push(&_pausemenu_state);
+    _pausemenu_state.start();
+    g_music_manager.pause_fx();
+  }
 }
 
 void GameEngine::set_state_ingame()
 {
-    CommonResources *common_resources = common_resources_get_instance();
-    common_resources -> p_current_player = &(common_resources -> player1);
-    _states_stack.push(&_ingame_state);
+  CommonResources *common_resources = common_resources_get_instance();
+  common_resources -> p_current_player = &(common_resources -> player1);
+  _states_stack.push(&_ingame_state);
+  g_music_manager.unpause_fx();
 }
 
 void GameEngine::set_state_gameover(int mode)
 {
-    _gameover_state.set_mode(mode);
-    _gameover_state.start();
-    _states_stack.push(&_gameover_state);
+  _gameover_state.set_mode(mode);
+  _gameover_state.start();
+  _states_stack.push(&_gameover_state);
 }
 
 /*void GameEngine::set_state_highscore()
- {
-         states_stack.push(&highscore_state);
- }*/
+  {
+  states_stack.push(&highscore_state);
+  }*/
 
 
 void GameEngine::set_state_options_menu()
 {
-    if (_states_stack.top() != &_optionsmenu_state)
-    {
-        _states_stack.push(&_optionsmenu_state);
-        _optionsmenu_state.start();
-        _pausemenu_state.start();
-    }
+  if (_states_stack.top() != &_optionsmenu_state)
+  {
+    _states_stack.push(&_optionsmenu_state);
+    _optionsmenu_state.start();
+    _pausemenu_state.start();
+  }
 }
 
 void GameEngine::set_state_skin_menu()
 {
-    if (_states_stack.top() != &_skinsmenu_state)
-    {
-        _states_stack.push(&_skinsmenu_state);
-        _skinsmenu_state.start();
-        _pausemenu_state.start();
-				_optionsmenu_state.start();
-		}
+  if (_states_stack.top() != &_skinsmenu_state)
+  {
+    _states_stack.push(&_skinsmenu_state);
+    _skinsmenu_state.start();
+    _pausemenu_state.start();
+    _optionsmenu_state.start();
+  }
 }
 
 void GameEngine::set_state_quit_menu(int action)
 {
-		if (_states_stack.top() != &_quitmenu_state)
-		{
-				_quitmenu_state.set_action(action);
-				_states_stack.push(&_quitmenu_state);
-				_pausemenu_state.start();
-				_quitmenu_state.start();
-		}
+  if (_states_stack.top() != &_quitmenu_state)
+  {
+    _quitmenu_state.set_action(action);
+    _states_stack.push(&_quitmenu_state);
+    _pausemenu_state.start();
+    _quitmenu_state.start();
+  }
 }
 
 void GameEngine::stop_current_state()
 {
-		_states_stack.pop();
+  _states_stack.pop();
+  if(_states_stack.top() == &_ingame_state)
+  {
+    g_music_manager.unpause_fx();
+  }
 }
 
 void GameEngine::change_screen_size()
 {
-		Preferences *pref = pref_get_instance();	
+  Preferences *pref = pref_get_instance();	
 		
-		int width = 800;
-		int height = 600;
-		bool wide = false;
+  int width = 800;
+  int height = 600;
+  bool wide = false;
 		
-		if(_render_mode == RENDER_OPENGL)
-		{
-				switch(pref -> screen_size)
-				{
-						case SCREEN_SIZE_320x240:
-								width = 320;
-								height = 240;
-								wide = true;
-								break;
+  if(_render_mode == RENDER_OPENGL)
+  {
+    switch(pref -> screen_size)
+    {
+    case SCREEN_SIZE_320x240:
+      width = 320;
+      height = 240;
+      wide = true;
+      break;
 								
-						case SCREEN_SIZE_640x480:
-								width = 640;
-								height = 480;
-								wide = false;
-								break;
+    case SCREEN_SIZE_640x480:
+      width = 640;
+      height = 480;
+      wide = false;
+      break;
 								
-						case SCREEN_SIZE_640x480_WIDE:
-								width = 640;
-								height = 480;
-								wide = true;
-								break;
+    case SCREEN_SIZE_640x480_WIDE:
+      width = 640;
+      height = 480;
+      wide = true;
+      break;
 								
-						case SCREEN_SIZE_800x600:
-								width = 800;
-								height = 600;
-								wide = false;
-								break;
+    case SCREEN_SIZE_800x600:
+      width = 800;
+      height = 600;
+      wide = false;
+      break;
 								
-						case SCREEN_SIZE_800x600_WIDE:
-								width = 800;
-								height = 600;
-								wide = true;
-								break;								
-				}
-		}
+    case SCREEN_SIZE_800x600_WIDE:
+      width = 800;
+      height = 600;
+      wide = true;
+      break;								
+    }
+  }
 		
-		if(pref -> fullscreen)
-		{
-				_p_window->set_fullscreen(width,height,0,0);
-				CL_Mouse::hide();
+  if(pref -> fullscreen)
+  {
+    _p_window->set_fullscreen(width,height,0,0);
+    CL_Mouse::hide();
 				
-				if(_render_mode == RENDER_OPENGL)
-				{
-						if(!wide)
-						{
-								CL_GraphicContext *gc = _p_window -> get_gc();
-								double scale_width = width / 800.0;
-								double scale_height = height / 600.0;
-								gc -> set_scale(scale_width, scale_height);
-						}
-						else
-						{
-								CL_Display::clear(CL_Color(0, 0, 0));
-							  CL_Display::flip();
+    if(_render_mode == RENDER_OPENGL)
+    {
+      if(!wide)
+      {
+	CL_GraphicContext *gc = _p_window -> get_gc();
+	double scale_width = width / 800.0;
+	double scale_height = height / 600.0;
+	gc -> set_scale(scale_width, scale_height);
+      }
+      else
+      {
+	CL_Display::clear(CL_Color(0, 0, 0));
+	CL_Display::flip();
 								
-								int new_width = 2 * width - 16.0 / 10.0 * height;
-								int dx = (width - new_width) / 2;								
+	int new_width = 2 * width - 16.0 / 10.0 * height;
+	int dx = (width - new_width) / 2;								
 
-								CL_GraphicContext *gc = _p_window -> get_gc();
-								double scale_width = new_width / 800.0;
-								double scale_height = height / 600.0;
-								gc -> set_scale(scale_width, scale_height);								
-								gc -> add_translate(dx, 0, 0);			
+	CL_GraphicContext *gc = _p_window -> get_gc();
+	double scale_width = new_width / 800.0;
+	double scale_height = height / 600.0;
+	gc -> set_scale(scale_width, scale_height);								
+	gc -> add_translate(dx, 0, 0);			
                 
-						}
+      }
 				
-				}
-		}
-		else
-		{
-				_p_window -> set_windowed();
-				CL_Mouse::show();		
+    }
+  }
+  else
+  {
+    _p_window -> set_windowed();
+    CL_Mouse::show();		
 				
-				if(_render_mode == RENDER_OPENGL)
-				{		
-						_p_window -> set_size(width, height);
+    if(_render_mode == RENDER_OPENGL)
+    {		
+      _p_window -> set_size(width, height);
 						
-						CL_GraphicContext *gc = _p_window -> get_gc();
-						double scale_width = width  / 800.0;
-						double scale_height = height / 600.0;
-						gc -> set_scale(scale_width, scale_height);						
+      CL_GraphicContext *gc = _p_window -> get_gc();
+      double scale_width = width  / 800.0;
+      double scale_height = height / 600.0;
+      gc -> set_scale(scale_width, scale_height);						
 						
-				}
-		}			
+    }
+  }			
 }
 
 
 void GameEngine::toggle_screen()
 {
-    Preferences *pref = pref_get_instance();
-    pref -> fullscreen = !pref -> fullscreen;		
-		change_screen_size();
-		_optionsmenu_state.toggle_screen();
+  Preferences *pref = pref_get_instance();
+  pref -> fullscreen = !pref -> fullscreen;		
+  change_screen_size();
+  _optionsmenu_state.toggle_screen();
 }
 
 void GameEngine::refresh_framerate_limit()
 {
-		Preferences *pref = pref_get_instance();
-		_fps_getter.set_fps_limit(pref -> maxfps);
+  Preferences *pref = pref_get_instance();
+  _fps_getter.set_fps_limit(pref -> maxfps);
 }
 
 int GameEngine::get_fps()
 {
-    return _fps_getter.get_fps();
+  return _fps_getter.get_fps();
 }
 
 bool GameEngine::is_opengl_used()
 {
-    return _render_mode;
+  return _render_mode;
 }
 
 bool GameEngine::is_fullscreen()
 {
-    Preferences *pref = pref_get_instance();
-    return pref -> fullscreen;
+  Preferences *pref = pref_get_instance();
+  return pref -> fullscreen;
 }
 
 void GameEngine::set_skin(std::string skin)
 {
-	  _p_loading_screen = my_new LoadingScreen();
-    _p_loading_screen -> set_progression(0.0f);
+  _p_loading_screen = my_new LoadingScreen();
+  _p_loading_screen -> set_progression(0.0f);
 	
-    CommonResources *resources = common_resources_get_instance();
-    Preferences *pref = pref_get_instance();
+  CommonResources *resources = common_resources_get_instance();
+  Preferences *pref = pref_get_instance();
 
-    std::string old_skin = pref -> skin;
+  std::string old_skin = pref -> skin;
 
-    try
+  try
+  {
+    pref -> skin = skin;
+
+    if(_running)
     {
-        pref -> skin = skin;
-
-        if(_running)
-        {
-            resources -> load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(2.0f / 12.0f);
-        }
-
-        if(_running)
-        {
-            _title_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(3.0f / 12.0f);
-        }
-
-        if(_running)
-        {
-            _common_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(4.0f / 12.0f);
-        }
-
-        if(_running)
-        {
-            _ingame_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(5.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _gameover_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(6.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _pausemenu_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(7.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _skinsmenu_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(8.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _optionsmenu_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(9.0f / 12.0f);
-        }
-
-        if(_running)
-        {
-            _optionsmenu_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(10.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _title_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(11.0f / 12.0f);
-        }
-
-
-        if(_running)
-        {
-            _quitmenu_state.load_gfx(pref -> skin);
-            _p_loading_screen -> set_progression(12.0f / 12.0f);
-        }
-
-        pref -> write();
-
+      resources -> load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(2.0f / 12.0f);
     }
-    catch (CL_Error err)
+
+    if(_running)
     {
-        std::cout << "Skin error : " << err.message << std::endl;
-        std::cout << "Error in : " << skin << std::endl;
-        if (old_skin.compare(skin))
-        {
-            std::cout << "Now loading default skin." << std::endl;
-            skin = get_skins_path() + get_path_separator() + "aqua.zip";
-            set_skin(skin);
-        }
-        else
-        {
-            throw err;
-        }
+      _title_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(3.0f / 12.0f);
     }
+
+    if(_running)
+    {
+      _common_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(4.0f / 12.0f);
+    }
+
+    if(_running)
+    {
+      _ingame_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(5.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _gameover_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(6.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _pausemenu_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(7.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _skinsmenu_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(8.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _optionsmenu_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(9.0f / 12.0f);
+    }
+
+    if(_running)
+    {
+      _optionsmenu_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(10.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _title_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(11.0f / 12.0f);
+    }
+
+
+    if(_running)
+    {
+      _quitmenu_state.load_gfx(pref -> skin);
+      _p_loading_screen -> set_progression(12.0f / 12.0f);
+    }
+
+    pref -> write();
+
+  }
+  catch (CL_Error err)
+  {
+    std::cout << "Skin error : " << err.message << std::endl;
+    std::cout << "Error in : " << skin << std::endl;
+    if (old_skin.compare(skin))
+    {
+      std::cout << "Now loading default skin." << std::endl;
+      skin = get_skins_path() + get_path_separator() + "aqua.zip";
+      set_skin(skin);
+    }
+    else
+    {
+      throw err;
+    }
+  }
 	
-		my_delete(_p_loading_screen);
-		_p_loading_screen = NULL;
+  my_delete(_p_loading_screen);
+  _p_loading_screen = NULL;
 }
 
 void GameEngine::set_skin_element(u_int element)
 {
-    _skinsmenu_state.set_skin_elements(element);
+  _skinsmenu_state.set_skin_elements(element);
 }
--- trunk/src/MusicManager.cpp
+++ trunk/src/MusicManager.cpp
@@ -24,6 +24,7 @@ MusicManager::MusicManager()
   for(int i=0; i<NB_SOUNDS; ++i)
   {
     _sounds_p[i] = NULL;
+    _sounds_position[i] = 0;
   }
 }
 
@@ -45,8 +46,14 @@ void MusicManager::init()
   _playback.set_looping(true);
   _playback.play();
   _playback.set_volume(p_pref -> music_level / 100.f);
-//  _p_sound = my_new CL_SoundBuffer("sounds/ping2.wav", false);
   _sounds_p[SOUND_MOVE] = my_new CL_SoundBuffer("sounds/move.wav", false);
+  _sounds_p[SOUND_FALL] = my_new CL_SoundBuffer("sounds/fall.wav", false);
+  _sounds_p[SOUND_CREATION] = my_new CL_SoundBuffer("sounds/creation.wav", false);
+  _sounds_p[SOUND_DESTROY] = my_new CL_SoundBuffer("sounds/destroy.wav", false);
+  _sounds_playback[SOUND_MOVE] = _sounds_p[SOUND_MOVE]->prepare();
+  _sounds_playback[SOUND_FALL] = _sounds_p[SOUND_FALL]->prepare();
+  _sounds_playback[SOUND_CREATION] = _sounds_p[SOUND_CREATION]->prepare();
+  _sounds_playback[SOUND_DESTROY] = _sounds_p[SOUND_DESTROY]->prepare();
 }
 
 void MusicManager::term()
@@ -96,11 +103,36 @@ void MusicManager::play_sound(int sound_index)
 {
   if(_sounds_p[sound_index])
   {
-    _sounds_p[sound_index] -> play();
+    _sounds_playback[sound_index].set_position(0);
+    _sounds_playback[sound_index].play();
   }
 }
 
 
+void MusicManager::pause_fx()
+{
+  for(int i=0; i<NB_SOUNDS; ++i)
+  {
+    if(_sounds_p[i] && _sounds_playback[i].is_playing())
+    {
+      _sounds_position[i] = _sounds_playback[i].get_position();
+      _sounds_playback[i].stop();
+    }   
 
+  }
+}
 
+void MusicManager::unpause_fx()
+{
+  for(int i=0; i<NB_SOUNDS; ++i)
+  {
+    if(_sounds_p[i] && _sounds_position[i] > 0)
+    {
+      _sounds_playback[i].set_position(_sounds_position[i]);
+      _sounds_playback[i].play();
+      _sounds_position[i] = 0;
+    }  
+
+  }
+}
 
